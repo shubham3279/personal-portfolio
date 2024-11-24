56ceb49f293b921e84ee0dc87a258ad5c47965dc
Shubham Kumar <shubhamv0918@gmail.com>
2024-11-24 12:50:49 +0000

    created wide mode and changed background color

--- page_1.py
+++ page_1.py
@@ -2,13 +2,22 @@ import streamlit as st
 import numpy as np
 
 
+
+# App content
 st.title("EDUCATION")
 
+# Create tabs
 tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
+
+# Generate random data
 data = np.random.randn(10, 1)
 
-tab1.subheader("A tab with a chart")
-tab1.line_chart(data)
+# Content for the first tab
+with tab1:
+    st.subheader("A tab with a chart")
+    st.line_chart(data)
 
-tab2.subheader("A tab with the data")
-tab2.write(data)
+# Content for the second tab
+with tab2:
+    st.subheader("A tab with the data")
+    st.write(data)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
+# page-configurations
+st.set_page_config(layout="wide")
+
+
 
 with st.sidebar:
     st.write(
