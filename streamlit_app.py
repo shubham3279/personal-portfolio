70cc37fd344237a0b4dc89048c7b50a62c774384
Shubham Kumar <shubhamv0918@gmail.com>
2024-11-24 12:41:50 +0000

    added demo pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
 
-st.title("PORTFOLIO")
 
+with st.sidebar:
+    st.write(
+        'Hi, I am Shubham.'
+    )
+
+pg = st.navigation([st.Page("page_1.py"), st.Page("page_2.py")])
+pg.run()
